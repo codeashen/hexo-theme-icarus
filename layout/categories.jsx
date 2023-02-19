531991e96efa97a90175c4e960d5a721b4f0a671
CodeAshen <codeashen@foxmail.com>
2023-02-19 23:14:07 +0800

    style: optimize layout and icons

--- layout/categories.jsx
+++ layout/categories.jsx
@@ -1,10 +1,36 @@
-const { Component } = require('inferno');
+const { Component, Fragment } = require('inferno');
 const Categories = require('hexo-component-inferno/lib/view/widget/categories');
+const Index = require('./index');
 
 module.exports = class extends Component {
-    render() {
-        const { site, page, helper } = this.props;
+    // render() {
+    //     const { site, page, helper } = this.props;
 
-        return <Categories.Cacheable site={site} page={page} helper={helper} />;
+    //     return <Categories.Cacheable site={site} page={page} helper={helper} />;
+    // }
+
+    render() {
+        const { config, page, helper } = this.props;
+        const { url_for, _p } = helper;
+
+        // This is just like a two-row table.
+        return <Fragment>
+            <Index config={config} page={page} helper={helper} />
+            {/* Remove category for the category page. */}
+            <div class="card">
+                {/* <div class="card-content">
+                    <nav class="breadcrumb" aria-label="breadcrumbs">
+                        <ul>
+                            <li><a href={url_for('/categories')}>{_p('common.category', Infinity)}</a></li>
+                            {page.parents.map(category => {
+                                return <li><a href={url_for(category.path)}>{category.name}</a></li>;
+                            })}
+                            <li class="is-active"><a href="#" aria-current="page">{page.category}</a></li>
+                        </ul>
+                    </nav>
+                </div> */}
+            </div>
+            {/* <Index config={config} page={page} helper={helper} /> */}
+        </Fragment>;
     }
 };
